94e7cae9005d9b1d2c05e641e680495cc51c078f
GeorgeAlfonso <280772butakov@gmail.com>
2023-10-05 02:59:32 +1000

    init

---
+++ config.cpp
@@ -0,0 +1,18 @@
+#define _ARMA_
+
+class CfgPatches
+{
+	class division7_misc
+	{
+		author = "Division-7";
+		url = "https://discord.gg/c2BgeFMgqk";
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"division7_core"};
+	};
+};
+
+
---
+++ markers/config.cpp
@@ -0,0 +1,74 @@
+#define _ARMA_
+
+class CfgPatches
+{
+	class division7_markers
+	{
+		author = "Division-7";
+		url = "https://discord.gg/c2BgeFMgqk";
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+		ammo[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"division7_core"};
+	};
+};
+
+class CfgMarkers
+{
+	class div7_logo
+	{
+		name = "Division-7";
+		icon = "division7_misc\markers\icons\Division7.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+	class div7_crimsonTeam_logo
+	{
+		name = "Crimson Team";
+		icon = "division7_misc\markers\icons\CrimsonTeam.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+	class div7_thanatos_logo
+	{
+		name = "Thanatos";
+		icon = "division7_misc\markers\icons\Thanatos.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+	class div7_hellhounds_logo
+	{
+		name = "Hellhounds";
+		icon = "division7_misc\markers\icons\Hellhounds.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+	class div7_honeybadger_logo
+	{
+		name = "Honey Badger";
+		icon = "division7_misc\markers\icons\HoneyBadgers.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+	class div7_burevestiki_logo
+	{
+		name = "Thunderbird";
+		icon = "division7_misc\markers\icons\Burevestnik.paa";
+		color[] = {0, 0, 0, 1};
+		size = 40;
+		shadow = 0;
+		scope = 2;
+	};
+};
